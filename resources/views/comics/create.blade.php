bfd357392cf8ed43711b73ca6bda9a7851c39dbe
vtnboolean <vnthuanx@gmail.com>
2022-09-16 05:44:59 +0200

    "add new comic"  link added

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -4,56 +4,60 @@
 
 @section('main-content')
 
- <div class="col-6 p-3">
+<section class="my-background">
 
-    {{-- Specificazione ROTTAe METDO --}}
-    <form action="{{ route('comics.store') }}" method='post'>
-    {{-- INSERIMENTO del XSRF --}}
-    @csrf
+    <div class="col-6 p-3 mx-auto">
 
-        <div class="form-group py-3 py-3">
-        <label for="input-title">Title</label>
-        <input type="text" class="form-control" id="input-title" name="title">
-        <small id="emailHelp" class="form-text text-muted">Insert your title</small>
-        </div>
+        {{-- Specificazione ROTTAe METDO --}}
+        <form action="{{ route('comics.store') }}" method='post'>
+        {{-- INSERIMENTO del XSRF --}}
+        @csrf
 
-        <div class="form-group py-3">
-        <label for="input-sale-date">Sale Date</label>
-        <input type="date" class="form-control" id="input-sale-date" name="sale_date">
-        </div>
+            <div class="form-group py-3 py-3">
+            <label for="input-title">Title</label>
+            <input type="text" class="form-control" id="input-title" name="title">
+            <small id="emailHelp" class="form-text text-muted">Insert your title</small>
+            </div>
 
-        <div class="form-group py-3">
-            <label for="input-description">Description</label>
-            <textarea class="form-control" name="description" id="input-description" cols="30" rows="8"></textarea>
-        </div>
+            <div class="form-group py-3">
+            <label for="input-sale-date">Sale Date</label>
+            <input type="date" class="form-control" id="input-sale-date" name="sale_date">
+            </div>
 
-        <div class="form-group py-3">
-            <label for="input-price">Price</label>
-            <input type="number" class="form-control" id="input-price" name="price">
-        </div>
+            <div class="form-group py-3">
+                <label for="input-description">Description</label>
+                <textarea class="form-control" name="description" id="input-description" cols="30" rows="8"></textarea>
+            </div>
 
-        <div class="form-group py-3">
-        <label for="input-thumb">Thumbnail</label>
-        <input type="đate" class="form-control" id="input-thumb" name="thumb">
-        </div>
+            <div class="form-group py-3">
+                <label for="input-price">Price</label>
+                <input type="number" class="form-control" id="input-price" name="price">
+            </div>
 
-        <div class="form-group py-3">
-            <label for="input-series">Series</label>
-            <input type="text" class="form-control" id="input-series" name="series">
-        </div>
+            <div class="form-group py-3">
+            <label for="input-thumb">Thumbnail</label>
+            <input type="đate" class="form-control" id="input-thumb" name="thumb">
+            </div>
 
-        <div class="form-group py-3">
-            <label for="input-type">Type</label>
-            <input type="text" class="form-control" id="input-type" name="type">
-        </div>
+            <div class="form-group py-3">
+                <label for="input-series">Series</label>
+                <input type="text" class="form-control" id="input-series" name="series">
+            </div>
 
-        <div class="form-group py-3 text-center">
-            <button type="submit" class="btn btn-lg btn-secondary">Send your comic</button>
-        </div>
+            <div class="form-group py-3">
+                <label for="input-type">Type</label>
+                <input type="text" class="form-control" id="input-type" name="type">
+            </div>
 
-    </form>
+            <div class="form-group py-3 text-center">
+                <button type="submit" class="btn btn-lg btn-secondary">Send your comic</button>
+            </div>
 
- </div>
+        </form>
+
+     </div>
+
+</section>
 
 
 @endsection
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -7,19 +7,33 @@
 
             <img class="logo-comics" src="assets/images/logo-comics.png" >
 
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarTogglerDemo03">
 
-                <ul class="navbar-nav my-navbar-nav mt-2 mt-lg-0 pr-5">
-                    <li class="nav-item active">
-                    <a class="nav-link  my-navlink" href="{{ route('home.index') }}">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link  my-navlink" href="{{ route('comics.index') }}">Comics</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link my-navlink" href="{{ route('contacts') }}">Contacts</a>
-                    </li>
+                <ul class="navbar-nav my-navbar-nav mt-2 mt-lg-0 ">
+
+                    <div class="d-flex justify-content-start">
+                        <li class="nav-item">
+                            <a class="nav-link  my-navlink {{ request()->routeIs('home.index') ? 'active' : '' }}"
+                               href="{{ route('home.index') }}">Home <span class="sr-only">(current)</span></a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link  my-navlink   {{ request()->routeIs('comics.index') ? 'active' : '' }}"
+                               href="{{ route('comics.index') }}">Comics</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link my-navlink" href="{{ route('contacts') }}">Contacts</a>
+                        </li>
+
+                    </div>
+
                 </ul>
+
+                <div class="nav-item">
+                    <a class="nav-link  my-navlink  {{ request()->routeIs('comics.create') ? 'active' : '' }}"
+                       href="{{ route('comics.create') }}">Add New Comic</a>
+                </div>
 
             </div>
 
